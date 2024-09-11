5df68d9d223a3b71f1c0aa87bbfb7d2f11a5c7fc
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-09-11 16:23:37 +0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@ import withVideos from 'next-videos';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+};
 
 // Объединение плагинов
 const combinedConfig = withNextIntl(withVideos(nextConfig));
