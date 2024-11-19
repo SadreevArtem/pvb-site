b5c890e4e15c23782dbb168b315a09d023e3e999
123 <a123@123s-MacBook-Air.local>
2024-11-19 17:59:47 +0500

    add programm

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,7 @@
-import { useLocale, useTranslations } from 'next-intl';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
-
+import { useLocale, useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   const t = useTranslations("Footer");
@@ -10,7 +9,7 @@ const Footer = () => {
   return (
     <footer className="w-full pt-4 md:mt-[60px] mt-4 xxl:px-24 max-sm:hidden">
       <div className="container grid md:grid-cols-4 md:gap-8 gap-4 md:mb-24">
-        <div className='text-sm flex flex-col gap-[12px]'>
+        <div className="text-sm flex flex-col gap-[12px]">
           <Link href="/" className="relative block shrink-0">
             <Image
               src="/logo-max.png"
@@ -20,48 +19,83 @@ const Footer = () => {
               className="relative max-md:-left-4"
             />
           </Link>
-          <p className='block'>© 2024 ИП Бондаренко В. В.</p>
-          <p className='block'>Procurement Victor Bondarenko</p>
-          <p className='block'>ИНН 503212020713</p>
-          <p className='block'>E-mail: <Link className='text-link' href={"mailto:info@ppvb.pro"}>info@ppvb.pro</Link></p>
+          <p className="block">© 2024 ИП Бондаренко В. В.</p>
+          <p className="block">Procurement Victor Bondarenko</p>
+          <p className="block">ИНН 503212020713</p>
+          <p className="block">
+            E-mail:{" "}
+            <Link className="text-link" href={"mailto:info@ppvb.pro"}>
+              info@ppvb.pro
+            </Link>
+          </p>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-primary font-extrabold uppercase text-[24px] mb-3">
             <Link href={`/${localActive}/consultation`}>{t("link1")}</Link>
           </h3>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/consultation`}>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/consultation`}
+          >
             <span>{t("consultationOne")}</span>
           </Link>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/consultation`}>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/consultation`}
+          >
             <span>{t("consultationTwo")}</span>
           </Link>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/consultation`}>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/consultation`}
+          >
             <span>{t("consultationThree")}</span>
           </Link>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/consultation`}>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/consultation`}
+          >
             <span>{t("consultationFour")}</span>
           </Link>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-primary font-extrabold uppercase text-[24px] mb-3">
-          <Link href={`/${localActive}/education`}>{t("link2")}</Link>
+            <Link href={`/${localActive}/education`}>{t("link2")}</Link>
           </h3>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/education`}>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/education`}
+          >
             <span>{t("educationOne")}</span>
           </Link>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/education`}>
-          <span>{t("educationTwo")}</span>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/education`}
+          >
+            <span>{t("educationTwo")}</span>
           </Link>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-primary font-extrabold uppercase text-[24px] mb-3">
-          <Link href={`/${localActive}/programs`}>{t("link3")}</Link>
+            <Link href={`/${localActive}/programs`}>{t("link3")}</Link>
           </h3>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/programs`}>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/programs`}
+          >
             <span>PVB-U ©</span>
           </Link>
-          <Link className="text-grayText hover:text-black" href={`/${localActive}/programs`}>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/programs`}
+          >
             <span>PVB Control ©</span>
+          </Link>
+          <Link
+            className="text-grayText hover:text-black"
+            href={`/${localActive}/programs`}
+          >
+            <span>PVB SpecFlow ©</span>
           </Link>
         </div>
       </div>
@@ -76,7 +110,5 @@ const Footer = () => {
       </div>
     </footer>
   );
-}
-
-
+};
 export default Footer;
