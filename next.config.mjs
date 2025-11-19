b0119749b60e23ca9c2831349bbbfa1a2dab018f
123 <a123@123s-MacBook-Air.local>
2025-11-19 15:45:41 +0500

    add accordion

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-import withVideos from 'next-videos';
+import createNextIntlPlugin from "next-intl/plugin";
+import withVideos from "next-videos";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+  output: "standalone",
   images: {
     unoptimized: true,
   },
 };
 
 // Объединение плагинов
-const combinedConfig = withNextIntl(withVideos(nextConfig));
-
-export default combinedConfig;
+module.exports = withNextIntl(withVideos(nextConfig));
