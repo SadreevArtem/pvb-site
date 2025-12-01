66df8eadb54c17f29848589635cd29461880c537
123 <a123@123s-MacBook-Air.local>
2025-12-01 19:33:43 +0500

    part 3 glossary

---
+++ src/app/[locale]/glossary/commercial-terms/page.tsx
@@ -0,0 +1,168 @@
+import {
+  accordeonItemsA,
+  accordeonItemsB,
+  accordeonItemsC,
+  accordeonItemsD,
+  accordeonItemsE,
+  accordeonItemsF,
+  accordeonItemsI,
+  accordeonItemsL,
+  accordeonItemsP,
+} from "@/app/static/commercial-terms";
+import { AppAccordionGroupWrapper } from "@/components/AppAccordionGroupWrapper/AppAccordionGroupWrapper";
+import { BackToTopButton } from "@/components/BackToTopButton/BackToTopButton";
+import { Feedback } from "@/components/Feedback/Feedback";
+import { GlossaryLinks } from "@/components/GlossaryLinks/GlossaryLinks";
+import { useLocale, useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const CommercialTerms = ({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) => {
+  unstable_setRequestLocale(locale);
+  const localActive = useLocale();
+  const t = useTranslations("CommercialTermsPage");
+  return (
+    <div className="min-h-[50vh] md:mt-[100px] mt-[86px] bg-white">
+      <div className="h-[140px] w-full relative">
+        <Image
+          src={"/background.png"}
+          alt={""}
+          width={1000}
+          height={100}
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="container mt-6">
+        <h1 className="md:text-[48px] md:ml-[260px] text-[36px] text-primary font-bold">
+          {t("title")}
+        </h1>
+      </div>
+      <div className="container mb-8 text-[18px] text-grayText">
+        <GlossaryLinks />
+        <p id="A" className="title md: text-[48px] md:ml-[160px]">
+          {"A"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsA}
+          />
+        </div>
+        <p id="B" className="title md: text-[48px] md:ml-[160px]">
+          {"B"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsB}
+          />
+        </div>
+        <p id="C" className="title md: text-[48px] md:ml-[160px]">
+          {"C"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsC}
+          />
+        </div>
+        <p id="D" className="title md: text-[48px] md:ml-[160px]">
+          {"D"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsD}
+          />
+        </div>
+        <p id="E" className="title md: text-[48px] md:ml-[160px]">
+          {"E"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsE}
+          />
+        </div>
+        <p id="F" className="title md: text-[48px] md:ml-[160px]">
+          {"F"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsF}
+          />
+        </div>
+        <p id="I" className="title md: text-[48px] md:ml-[160px]">
+          {"I"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsI}
+          />
+        </div>
+
+        <p id="L" className="title md: text-[48px] md:ml-[160px]">
+          {"L"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsL}
+          />
+        </div>
+
+        <p id="P" className="title md: text-[48px] md:ml-[160px]">
+          {"P"}
+        </p>
+        <div className="mt-8">
+          <AppAccordionGroupWrapper
+            currentTranslate="CommercialTermsPage"
+            items={accordeonItemsP}
+          />
+        </div>
+
+        <div className="md:ml-[160px] mt-4">
+          <ul>
+            <li className="indent-8">
+              <Link
+                className="header-link"
+                href={`/${localActive}/glossary/valve-terms`}
+              >
+                {t("link1")}
+              </Link>
+            </li>
+            <li className="indent-8">
+              <Link
+                className="header-link"
+                href={`/${localActive}/glossary/control-terms`}
+              >
+                {t("link2")}
+              </Link>
+            </li>
+            <li className="indent-8">
+              <Link
+                className="header-link"
+                href={`/${localActive}/glossary/commercial-terms`}
+              >
+                {t("link3")}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <Feedback />
+      <BackToTopButton label={t("backToTop")} />
+    </div>
+  );
+};
+
+export default CommercialTerms;
---
+++ src/app/static/commercial-terms/index.ts
@@ -0,0 +1,124 @@
+export const accordeonItemsA = [
+  {
+    id: 115,
+    name: "t115",
+    value: ["p115"],
+  },
+];
+export const accordeonItemsB = [
+  {
+    id: 116,
+    name: "t116",
+    value: ["p116"],
+  },
+  {
+    id: 117,
+    name: "t117",
+    value: ["p117"],
+  },
+  {
+    id: 118,
+    name: "t118",
+    value: ["p118"],
+  },
+  {
+    id: 119,
+    name: "t119",
+    value: ["p119"],
+  },
+];
+export const accordeonItemsC = [
+  {
+    id: 120,
+    name: "t120",
+    value: ["p120"],
+  },
+  {
+    id: 121,
+    name: "t121",
+    value: ["p121"],
+  },
+  {
+    id: 122,
+    name: "t122",
+    value: ["p122"],
+  },
+  {
+    id: 123,
+    name: "t123",
+    value: ["p123"],
+  },
+  {
+    id: 124,
+    name: "t124",
+    value: ["p124"],
+  },
+];
+export const accordeonItemsD = [
+  {
+    id: 125,
+    name: "t125",
+    value: ["p125"],
+  },
+  {
+    id: 126,
+    name: "t126",
+    value: ["p126"],
+  },
+];
+export const accordeonItemsE = [
+  {
+    id: 127,
+    name: "t127",
+    value: ["p127"],
+  },
+  {
+    id: 128,
+    name: "t128",
+    value: ["p128"],
+  },
+];
+export const accordeonItemsF = [
+  {
+    id: 129,
+    name: "t129",
+    value: ["p129"],
+  },
+  {
+    id: 130,
+    name: "t130",
+    value: ["p130"],
+  },
+  {
+    id: 131,
+    name: "t131",
+    value: ["p131"],
+  },
+];
+
+export const accordeonItemsI = [
+  {
+    id: 132,
+    name: "t132",
+    value: ["p132"],
+  },
+];
+export const accordeonItemsL = [
+  {
+    id: 133,
+    name: "t133",
+    value: ["p133"],
+  },
+];
+export const accordeonItemsP = [
+  {
+    id: 134,
+    name: "t134",
+    value: ["p134"],
+  },
+  {
+    id: 135,
+    name: "t135",
+    value: ["p135"],
+  },
+];
